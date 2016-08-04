0e817b191673463753b1df8f6f89525535ad48ed
Matt Wilkin and Meaghan Jones <matt+meaghan@>
2016-08-04 13:17:06 -0700

    edit gulp file to include scss and also add a scss folder and styles file

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,8 @@ var lib = require('bower-files')({
 });
 var browserSync = require('browser-sync').create();
 var buildProduction = utilities.env.production;
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task("build", ['clean'], function(){
@@ -28,9 +30,10 @@ gulp.task("build", ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', ['build'], function() {
   browserSync.init({
     server: {
       baseDir: "./",
@@ -40,6 +43,16 @@ gulp.task('serve', function() {
 
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./build/css'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
